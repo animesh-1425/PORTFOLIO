f223623b21fff0d2c63d4d7fc9ecedacbd1f519c
animesh-1425 <animesh1425@gmail.com>
2024-02-29 01:11:50 +0530

    navbar

--- script.js
+++ script.js
@@ -15,4 +15,17 @@ icon.onclick=function(){
   }
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var navbar = document.querySelector('.container1');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) {
+        navbar.classList.add('navbar-bg');
+      } else {
+          navbar.classList.remove('navbar-bg');
+      }
+  });
+});
+
 //cvdownload
